9c26ebd882e6e18fc5bcd48dcfe9ae79c56e7336
jiangchao <explosionzing@126.com>
2022-10-24 10:48:47 +0800

    ✨ feat:新增日志功能

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
-const { PORT } = require('./config')
+const logger = require('./utils/logger')
 
+const { PORT } = require('./config')
 const { NotFound } = require('./utils/notFound')
 const catchError = require('./utils/catchError')
 
@@ -17,13 +18,21 @@ app.use(
 // 全局错误处理
 app.use(catchError)
 
-// 404
+// logger
 app.use(async (ctx, next) => {
-  await NotFound(ctx)
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  logger.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
 // routers
 const blog = require('./routes/blog')
 app.use(blog.routes(), blog.allowedMethods())
+
+// 404
+app.use(async (ctx, next) => {
+  await NotFound(ctx)
+})
 
 app.listen(PORT, () => console.log(`server is listening in ${PORT}`))
