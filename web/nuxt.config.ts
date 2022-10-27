2d1773f394d1dae3cca19626c0c85ac2e66fea5f
jiangchao <explosionzing@126.com>
2022-10-27 13:50:52 +0800

    ✨ feat:新增baseURL  /blog/

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
   // styleResources: {
   //   scss: ['@/assets/scss/base.scss']
   // },
+  app: {
+    baseURL: '/blog/'
+  },
   modules: ['cookie-universal-nuxt'],
   buildModules: ['@pinia/nuxt'],
   css: ['@/assets/scss/global.scss'],
