adce5ccbd4e19e01e0026798de78161cedf2e5a1
jiangchao <explosionzing@126.com>
2022-10-31 10:33:52 +0800

    🐎 ci:ci配置调整14

--- web/dockerBootstrap.sh
+++ web/dockerBootstrap.sh
@@ -3,20 +3,20 @@ echo -e "---------登录镜像容器服务--------"
 docker login --username=$1 registry.cn-hangzhou.aliyuncs.com --password=$2
 echo -e "---------停掉镜像--------"
 # 停掉容器
-docker stop jiangchaow
+docker stop nuxtblogweb
 echo -e "---------删除本地容器和镜像--------"
 # 删除本地容器
-docker rm jiangchaow
+docker rm nuxtblogweb
 # 删除本地镜像
-docker rmi registry.cn-hangzhou.aliyuncs.com/jiangchaow/jiangchaow:latest
+docker rmi registry.cn-hangzhou.aliyuncs.com/jiangchaow/nuxtblogweb:latest
 echo -e "---------拉取镜像--------"
 # 拉取镜像
-docker pull registry.cn-hangzhou.aliyuncs.com/jiangchaow/jiangchaow:latest
+docker pull registry.cn-hangzhou.aliyuncs.com/jiangchaow/nuxtblogweb:latest
 echo -e "---------创建容器并运行容器--------"
 # -rm: docker会在容器退出时删除与它关联的数据卷
 # -d: 后台运行容器，并返回容器ID
 # -p: 端口映射，本机端口:容器端口
 # --name: 指定容器名称
 # 最后一个为镜像名称
-docker run --rm -d -p 8080:80 --name jiangchaow registry.cn-hangzhou.aliyuncs.com/jiangchaow/jiangchaow:latest
+docker run --rm -d -p 9999:80 --name nuxtblogweb registry.cn-hangzhou.aliyuncs.com/jiangchaow/nuxtblogweb:latest
 echo -e "---------执行完毕--------"
