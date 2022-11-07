fea1b28c26e335d51cce04d794bf562e307566be
jiangchao <explosionzing@126.com>
2022-11-07 14:55:26 +0800

    🐞 fix:普通用户天机自动登录

--- web/app.vue
+++ web/app.vue
@@ -2,15 +2,32 @@
   <NuxtPage />
 </template>
 <script lang="ts" setup>
-import { ID_INJECTION_KEY } from 'element-plus'
+import { ID_INJECTION_KEY, ElMessage } from 'element-plus'
 import mousemove from '~~/utils/mousemove'
+import { login } from '~~/utils/api'
+import useBlogState from '~~/store'
+const store = useBlogState()
+const router = useRouter()
 provide(ID_INJECTION_KEY, {
   prefix: 100,
   current: 0
 })
-onMounted(() => {
+// 普通用户自动登录
+const loginFun = async () => {
+  let res = await login({ username: 'admin', password: 'admin' })
+  if (res.errorCode === 0) {
+    store.updateToken(res.data.token || '')
+  } else {
+    ElMessage({
+      message: res.message,
+      type: 'error'
+    })
+  }
+}
+onMounted(async () => {
   const ele = document.body
   mousemove(ele)
+  loginFun()
 })
 </script>
 <style lang="scss">
