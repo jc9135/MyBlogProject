2afb91bcad27c757fada4ea2ecbba0f7c01909f5
jiangchao <explosionzing@126.com>
2022-11-08 22:08:30 +0800

    🐞 fix:关闭热重载

--- web/ecosystem.config.js
+++ web/ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs',
-      watch: true, //启动热重载
+      watch: false, //启动热重载
       env: {
         // 环境参数，当前指定为生产环境 process.env.NODE_ENV
         NODE_ENV: 'production'
